7643672bd58f510c8541c1d38fc2598a3001fed3
birandkoray <birandkoray@gmail.com>
2019-01-07 21:09:38 +0300

    apollo server upgraded to version 2. graphql mutation & query example added

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,26 +1,51 @@
 const graphql = require('graphql');
+
 const PaintingType = require('./PaintingType');
-const Painting = require('./../models/Painting');
+
+const Painting = require('../models/Painting');
 
 const {
-		GraphQLObjectType,
-		GraphQLString,
-		GraphQLSchema
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLSchema,
+	GraphQLList
 } = graphql;
 
 const RootQuery = new GraphQLObjectType({
-		name: 'RootQueryType',
-		fields: {
-				painting: {
-						type: PaintingType,
-						args: { id: { type: GraphQLString } },
-						resolve(parent, args){
-								return Painting.findById(args.id)
-						}
-				}
+	name: 'RootQueryType',
+	fields: {
+		painting: {
+			type: PaintingType,
+			args: { id : {type : GraphQLString } },
+			resolve(parent, args){
+				return Painting.findById(args.id)
+			}
+		},
+		allPaintings: {
+			type: new GraphQLList(PaintingType),
+			resolve(parent ,args) {
+				return Painting.find()
+			}
 		}
-});
+	}
+})
+
+const Mutation = new GraphQLObjectType({
+	name: 'Mutation',
+	fields: {
+		createPainting: {
+			type: PaintingType,
+			args: {name: { type: GraphQLString} , url:{ type: GraphQLString }},
+			resolve(parent,args){
+				const painting = new Painting({name : args.name , url : args.url})
+				return painting.save()
+			}
+		}
+	}
+})
+
 
 module.exports = new GraphQLSchema({
-		query: RootQuery
-});
+	query: RootQuery,
+	mutation: Mutation
+})
--- index.js
+++ index.js
@@ -1,71 +1,51 @@
 const hapi = require('hapi');
 const mongoose = require('mongoose');
-const { graphqlHapi, graphiqlHapi } = require('apollo-server-hapi');
+const Painting = require('./models/Painting')
+const {ApolloServer } =require('apollo-server-hapi')
 const schema = require('./graphql/schema');
-const Painting = require('./models/Painting');
-
-/* swagger section */
-const Inert = require('inert');
-const Vision = require('vision');
-const HapiSwagger = require('hapi-swagger');
-const Pack = require('./package');
 
 
-const server = hapi.server({
-	port: 4000,
-	host: 'localhost'
+const Inert = require('inert')
+const Vision = require('vision')
+const HapiSwagger = require('hapi-swagger')
+const Pack = require('./package')
+
+
+mongoose.connect('<given mlab url>',
+	{ useNewUrlParser : true});
+
+mongoose.connection.once('open' , () => {
+	console.log('connected to DB')
+})
+
+const server = new ApolloServer({
+    schema
+  });
+
+const app = hapi.server({
+  port: 4000,
+  host: "localhost"
 });
 
-mongoose.connect('mongodb://indrek:test@ds231090.mlab.com:31090/powerful-api');
 
-mongoose.connection.once('open', () => {
-	console.log('connected to database');
-});
 
 const init = async () => {
 
-	await server.register([
+	await app.register([
 		Inert,
 		Vision,
 		{
 			plugin: HapiSwagger,
 			options: {
 				info: {
-					title: 'Paintings API Documentation',
+					title: 'Paintings API Doc',
 					version: Pack.version
 				}
 			}
 		}
-	]);
+		])
 
-	await server.register({
-		plugin: graphiqlHapi,
-		options: {
-			path: '/graphiql',
-			graphiqlOptions: {
-				endpointURL: '/graphql'
-			},
-			route: {
-				cors: true
-			}
-		}
-	});
-
-	await server.register({
-		plugin: graphqlHapi,
-		options: {
-			path: '/graphql',
-			graphqlOptions: {
-				schema
-			},
-			route: {
-				cors: true
-			}
-		}
-	});
-
-
-	server.route([
+	app.route([
 		{
 			method: 'GET',
 			path: '/api/v1/paintings',
@@ -97,15 +77,9 @@ const init = async () => {
 		}
 	]);
 
-	await server.start();
-	console.log(`Server running at: ${server.info.uri}`);
+	await server.applyMiddleware({ app })
+	await app.start();
+	console.log(`Server running at: ${app.info.uri}`);
 };
 
-process.on('unHandledRejection', (err) => {
-	if (err) {
-		console.log(err);
-		process.exit(1);
-	}
-});
-
-init();
+init()
